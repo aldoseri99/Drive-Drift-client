109a194886dfc0b6b5a438695bc8de3a3ce36c9a
MuathAlsabbah <muath.alsabbah@gmail.com>
2024-11-10 14:46:15 +0300

    okay

--- src/components/ViewVehicles.jsx
+++ src/components/ViewVehicles.jsx
@@ -1,29 +1,22 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-import { useParams, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import VehicleCard from './VehicleCard'
 
 const ViewVehicles = ({ vehicles }) => {
-
-
-
-
-
-  
   return (
     <div className="viewVehicles">
       <h2>View All Vehicles</h2>
       <section className="container-grid">
         {vehicles.map((vehicle) => (
-          <VehicleCard
-            key={vehicle.id}
-            onClick={() => handleVehicleClick(vehicle.id)}
-            brand={vehicle.brand}
-            model={vehicle.model}
-            price={vehicle.price}
-            color={vehicle.color}
-            category={vehicle.category}
-            image={vehicle.image}
-          />
+          <Link to={`/vehicles/${vehicle.id}`} key={vehicle.id}>
+            <VehicleCard
+              brand={vehicle.brand}
+              model={vehicle.model}
+              price={vehicle.price}
+              color={vehicle.color}
+              category={vehicle.category}
+              image={vehicle.image}
+            />
+          </Link>
         ))}
       </section>
     </div>
