415ff1ed031cfb678854125bec1b286f5a1fe00c
MuathAlsabbah <muath.alsabbah@gmail.com>
2024-11-10 15:17:07 +0300

    pop

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,15 @@
 const Nav = () => {
-
+  return (
     <nav className="navbar navbar-dark">
-      <a className="btn btn-dark" href="/">
-        Home
-      </a>
-      <a className="btn btn-dark" href="/addvehicle">
-        Add Vehicle
-      </a>
-    </nav>
+    <a className="btn btn-dark" href="/">
+      Home
+    </a>
+    <a className="btn btn-dark" href="/addvehicle">
+      Add Vehicle
+    </a>
+  </nav>
   )
+  
 }
 
 export default Nav
