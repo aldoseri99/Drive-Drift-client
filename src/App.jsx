e93874782471a2b4e9836ababe5b7299841b91a2
xriixs <saiyedaziz38@gmail.com>
2024-11-11 09:18:54 +0300

    CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import './App.css'
-
+import "./App.css"
 const App = () => {
-  return(
-    <div>hi</div>
+  return (
+    <div className="Main">
+      <video autoPlay muted loop className="myVideo">
+        <source src="./Videos/Background.mp4" type="video/mp4" />
+      </video>
+      <div className="Name">
+        <h1>Drive & Drift</h1>
+      </div>
+    </div>
   )
 }
-  
-
 export default App
