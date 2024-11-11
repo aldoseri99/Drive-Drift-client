3549ca5a8c8c238f464a03a170f1b99908050bce
xriixs <saiyedaziz38@gmail.com>
2024-11-11 15:40:19 +0300

    delete videos folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,4 @@
 import "./App.css"
-// <<<<<<< HEAD
-// const App = () => {
-//   return (
-//     <div className="Main">
-//       <div className="Name">
-//         <h1>Drive & Drift</h1>
-//       </div>
-//     </div>
-//   )
-// }
-// export default App
-
-// {
-//   /* <video autoPlay muted loop className="myVideo">
-//         <source src="./Videos/Background.mp4" type="video/mp4" />
-//       </video> */
-// }
-
 import { Route, Routes } from "react-router-dom"
 import axios from "axios"
 import { useState, useEffect } from "react"
